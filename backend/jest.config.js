d00f76cac7c2cd8a51605f2c9cdc3af8bc481f9b
Krzysztof Wasinski <wasinapl@gmail.com>
2024-12-14 13:08:45 +0100

    update

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testRegex: '.spec.ts$',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/test/setup.ts'],
+        "moduleFileExtensions": [
+            "js",
+            "json",
+            "ts"
+        ],
+        "rootDir": "src",
+        "testRegex": ".*\\.spec\\.ts$",
+        "transform": {
+            "^.+\\.(t|j)s$": "ts-jest"
+        },
+        "collectCoverageFrom": [
+            "**/*.(t|j)s"
+        ],
+        "coverageDirectory": "../coverage",
+        "testEnvironment": "node"
 };
