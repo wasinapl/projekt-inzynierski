a1f96a40122ce320d53e0904097bb82e1d03568d
Krzysztof Wasinski <wasinapl@gmail.com>
2024-12-29 18:49:42 +0100

    add bull module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,23 @@ import { PrismaModule } from './prisma/prisma.module';
 import { KnowledgeModule } from './knowledge/knowledge.module';
 import { APP_FILTER } from '@nestjs/core';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-    imports: [AuthModule, PrismaModule, KnowledgeModule],
+    imports: [
+        AuthModule,
+        PrismaModule,
+        KnowledgeModule,
+        BullModule.forRoot({
+            redis: {
+                host: process.env.REDIS_URL,
+                port: 6379,
+            },
+        }),
+        BullModule.registerQueue({
+            name: 'my-queue',
+        }),
+    ],
     providers: [
         {
             provide: APP_FILTER,
